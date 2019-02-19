1e3c932db5f30dcf943cd537114210e5a864b7d3
Zenderable <stumski.karol@gmail.com>
2019-02-19 22:04:11 +0100

    Added factorial and fibonacci number algorithm

---
+++ Algorithms and data structures/Algorithms.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_and_data_structures
+{
+    class Algorithms
+    {
+        public static int fib(int n)
+        {
+            if ((n == 1) || (n == 2))
+                return 1;
+            else
+                return fib(n - 1) + fib(n - 2);
+        }
+
+        public static int factorial_recursive(int i)
+        {
+            if (i < 1)
+                return 1;
+            else
+                return i * factorial_recursive(i - 1);
+        }
+
+        public static int factorial_iteratively(int n)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+
+        }
+    }
+}
--- Algorithms and data structures/AlgorithmsDataStructures.cs
+++ Algorithms and data structures/AlgorithmsDataStructures.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Algorithms_and_data_structures
 {
@@ -11,7 +13,7 @@ namespace Algorithms_and_data_structures
     {
         static void Main(string[] args)
         {
-            int first_choice;
+            int first_choice, picked_algorithm=0, picked_implementation;
             Console.BackgroundColor = ConsoleColor.Gray;
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("************** KAROL STUMSKI ***************");
@@ -30,13 +32,63 @@ namespace Algorithms_and_data_structures
                 Console.Write("Type '1' or '2': ");
             if(first_choice == 1)
             {
-                //ListOfAlgorithms:
                 //list of all algorithms
+                Algorithms algorithms = new Algorithms();
+                Algorithm_list:
+                Console.WriteLine("1: Factorial");
+                Console.WriteLine("2: Fibonacci number");
+                Console.WriteLine("");
+                Console.Write("Pick number of algorithm: ");
+                while (!int.TryParse(Console.ReadLine(), out picked_algorithm) || !(picked_algorithm >= 1 && picked_algorithm <= 2))
+                    Console.Write("Type '1' or '2': ");
+                switch (picked_algorithm)
+                {
+
+                    case 1:
+                        Again:
+                        int factorial_number;
+                        Console.WriteLine("Type '1' if you want use iterative implementation of factorial");
+                        Console.WriteLine("Type '2' if you want use recursive implementation of factorial");
+                        Console.Write("Your choice: ");
+                        while (!int.TryParse(Console.ReadLine(), out picked_implementation) || !(picked_implementation >= 1 && picked_algorithm <= 2))
+                            Console.Write("Type from '1' or '2': ");
+                        switch (picked_implementation)
+                        {
+                            case 1:
+                                Console.Write("Type number: ");
+                                while (!int.TryParse(Console.ReadLine(), out factorial_number))
+                                    Console.WriteLine("Type number: ");
+                                Console.Write($"Factorial number is {Algorithms.factorial_iteratively(factorial_number)}");
+                                break;
+                            case 2:
+                                Console.Write("Type number: ");
+                                while (!int.TryParse(Console.ReadLine(), out factorial_number))
+                                    Console.WriteLine("Type number: ");
+                                Console.Write($"Factorial number is {Algorithms.factorial_recursive(factorial_number)}");
+                                break;
+                            default:
+                                Console.WriteLine("Wrong number! Try again.");
+                                goto Again;
+                        }
+                        break;
+                    case 2:
+                        int n;
+                        Console.Write("Type amount of fibonacci numbers: ");
+                        while (!int.TryParse(Console.ReadLine(), out n))
+                            Console.WriteLine("Type number: ");
+                        Console.Write($"n-th number is {Algorithms.fib(n)}");
+                        break;
+                    default:
+                        Console.WriteLine("Wrong number! Try again.");
+                        goto Algorithm_list;
+                }
+
             }
             else
             {
                 //ListOfDataStructures:
                 //list of all examples of data structures
+                Console.WriteLine("1: Stack (not working)");
             }
 
 
