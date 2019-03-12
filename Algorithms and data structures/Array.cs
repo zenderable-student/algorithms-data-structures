57c07438506b674efeb11519e0fa6107229aae05
Zenderable <stumski.karol@gmail.com>
2019-03-12 10:24:11 +0100

    Added empty classes

---
+++ Algorithms and data structures/Array.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_and_data_structures
+{
+    class Array
+    {
+    }
+}
